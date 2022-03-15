e21f5e435a273e0163ef5d1640b1c104fe891bf9
AXX <37358812+1ax@users.noreply.github.com>
2022-03-15 11:48:52 +0300

    добавил проверку чтобы запросы по строкам, по которым ушли запросы не повторялись если программа остановлена

--- requests/main.py
+++ requests/main.py
@@ -18,6 +18,7 @@ class SendRequest(Thread):
         self.queue = queue
         self.lock = lock
         self.next_request = False
+        # если запрос с таким ID уже был эта переменная принимает значиние Истина
 
     def run(self):
         """Запуск потока"""
@@ -40,12 +41,13 @@ class SendRequest(Thread):
                 logging.info(f"Ошибка: запрос с id {id} уже был направлен")
                 self.next_request = True
     
-    def autosave_request(self, list_url, payload):
+    def autosave_request(self, list_url, payload, number):
         with self.lock:
             with open("autosave.txt", "a") as f:
                 d = {
                     "url": list_url,
-                    "payload": payload
+                    "payload": payload,
+                    "number": number
                 }
                 l = json.dumps(d)
                 f.write("%s\n" % l)
@@ -64,17 +66,17 @@ class SendRequest(Thread):
             with open("autosave.txt", "w") as file:
                 file.writelines(lines2)
 
-    def send_request(self, line):
+    def send_request(self, jline):
 
         config_clients = {
             "foo": ["https://yachtclubparus.ru/test/", "https://yachtclubparus.ru/test/1/"],
             "bar": ["https://yachtclubparus.ru/test/", "https://yachtclubparus.ru/test/2/"],
             "baz": ["https://yachtclubparus.ru/test/2/", "https://yachtclubparus.ru/test/1/"]
         }
-        logging.basicConfig(filename='requests.log', level=logging.INFO,
-                            format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
+        # logging.basicConfig(filename='requests.log', level=logging.INFO,
+                            # format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
 
-        jline = json.loads(line.rstrip('\n'))
+        # jline = json.loads(line.rstrip('\n'))
         list_url = config_clients.get(jline['client_id'])
         # получаем список адресов из конфига для отправки запросов
         if jline['client_id'] and jline['payload'] and jline['client_id'] in config_clients.keys():
@@ -88,7 +90,7 @@ class SendRequest(Thread):
                     # начинаем перебирать адреса из конфига и отправлять на них последовательно запросы
                     logging.info(f"Отправляем запрос {jline['payload']} на адрес: {list_url[i]}")
                     response = requests.post(list_url[i], data=jline['payload'])
-                    self.autosave_request(list_url[i], jline['payload'])
+                    self.autosave_request(list_url[i], jline['payload'], jline['number'])
                     # сохраняем строку во временный файл
                     if response.status_code != 200:
                         # если вернулся не код 200 начинаем удваивать интервал времени и повторять запросы
@@ -103,42 +105,25 @@ class SendRequest(Thread):
                     # else:
                     #     logging.info(f"Запрос {jline['payload']} на адрес: {list_url[i]} успешно выполнен.")
                     self.remove_request(list_url[i], jline['payload'])
-                    logging.info(f"Запрос {jline['payload']} на адрес: {list_url[i]} успешно выполнен.")
+                    logging.info(f"Запрос {jline['payload']} на адрес: {list_url[i]} успешно выполнен. Строка {jline['number']} обработана")
             self.next_request = False
             # переключаем флаг, чтобы следующий запрос мог уйти
         else:
             logging.info(f"Обработка запроса: Client_ID: {jline['client_id']}, playload: {jline['payload']} - ошибка")
 
-    def send_request_autosave(self, line):
-
-        logging.basicConfig(filename='requests.log', level=logging.INFO,
-                                format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
-
-        jline = json.loads(line.rstrip('\n'))
-        logging.info(f"Отправляем запрос {jline['payload']} на адрес: {jline['url']}")
-        response = requests.post(jline['url'], data=jline['payload'])
-        if response.status_code != 200:
-            # если вернулся не код 200 начинаем удваивать интервал времени и повторять запросы
-            time_size = 1
-            while response.status_code != 200:
-                time.sleep(time_size)
-                time_size *= 2
-                logging.info(f"Увеличиваем время отправки запроса {jline['payload']} на адрес {jline['url']} до: {time_size} секунд")
-                response = requests.post(jline['url'], data=jline['payload'])
-            self.remove_request(jline['url'], jline['payload'])
-            logging.info(f"Запрос {jline['payload']} на адрес: {jline['url']} успешно выполнен.")
-
-
 def http_post():
     # смотрим есть ли сохраненные неотправленные запросы, если да, то отправляем в один поток, не заморчаиваемся
+    logging.basicConfig(filename='requests.log', level=logging.INFO,
+                                    format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
+    save_str = 0
+    # переменная в которой будет храниться максмальный номер строки, по которой уходили запросы
     if os.stat("autosave.txt").st_size:
         with open("autosave.txt", 'r') as fp:
             for n, line in enumerate(fp, 1):
-                logging.basicConfig(filename='requests.log', level=logging.INFO,
-                                        format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
                 jline = json.loads(line.rstrip('\n'))
                 logging.info(f"Отправляем запрос {jline['payload']} на адрес: {jline['url']}")
                 response = requests.post(jline['url'], data=jline['payload'])
+                save_str = max(save_str, jline['number'])
                 if response.status_code != 200:
                     time_size = 1
                     while response.status_code != 200:
@@ -160,10 +145,12 @@ def http_post():
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     lines = os.path.join(dir_path, "requests.txt")
-
     with open(lines, 'r') as fp:
         for n, line in enumerate(fp, 1):
-            queue.put(line)
+            if n > save_str:
+                line_number_row = json.loads(line.rstrip('\n'))
+                line_number_row['number'] = n
+                queue.put(line_number_row)
     queue.join()
 
 
