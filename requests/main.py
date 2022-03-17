0ffe93b6c9454bad376c1394aae738973f1bbde0
1ax <skomorohov@gmail.com>
2022-03-17 12:56:39 +0300

    PIP8, test new ubuntu

--- requests/main.py
+++ requests/main.py
@@ -10,6 +10,7 @@ from queue import Queue
 THREAD_COUNT = 4
 REQUEST_FILE = 'requests.txt'
 AUTOSAVE_FILE = 'autosave.txt'
+ID_REQUEST_FILE = 'id.txt'
 CLIENTS = {
     "foo": ["https://yachtclubparus.ru/test/", "https://yachtclubparus.ru/test/1/"],
     "bar": ["https://yachtclubparus.ru/test/", "https://yachtclubparus.ru/test/2/"],
@@ -24,7 +25,7 @@ class SendRequest(Thread):
         self.queue = queue
         self.lock = lock
         self.next_request = False
-        # если запрос с таким ID уже был эта переменная принимает значиние Истина
+        # если запрос с таким ID уже был эта переменная принимает значение Истина
 
     def run(self):
         """Запуск потока"""
@@ -36,17 +37,17 @@ class SendRequest(Thread):
             # Отправляем сигнал о том, что задача завершена
             self.queue.task_done()
 
-    def check_and_write_id(self, id):
+    def check_and_write_id(self, id_request):
         with self.lock:
-            ids = [w for w in Path("id.txt").read_text(encoding="utf-8").replace("\n", " ").split()]
-            if id not in ids:
-                file2 = open("id.txt", "a")
-                file2.write(str(id) + '\n')
+            ids = [w for w in Path(ID_REQUEST_FILE).read_text(encoding="utf-8").replace("\n", " ").split()]
+            if id_request not in ids:
+                file2 = open(ID_REQUEST_FILE, "a")
+                file2.write(str(id_request) + '\n')
                 file2.close()
             else:
-                logging.info(f"Ошибка: запрос с id {id} уже был направлен")
+                logging.info(f"Ошибка: запрос с id {id_request} уже был направлен")
                 self.next_request = True
-    
+
     def autosave_request(self, list_url, payload, number):
         with self.lock:
             with open(AUTOSAVE_FILE, "a") as f:
@@ -55,15 +56,15 @@ class SendRequest(Thread):
                     "payload": payload,
                     "number": number
                 }
-                l = json.dumps(d)
-                f.write("%s\n" % l)
+                h = json.dumps(d)
+                f.write("%s\n" % h)
 
-    def remove_request(self, list_url, payload):
+    def remove_request(self, list_url, payload, number):
         with self.lock:
             with open(AUTOSAVE_FILE, 'r') as fp:
                 for n, line1 in enumerate(fp, 1):
                     jline1 = json.loads(line1.rstrip('\n'))
-                    if jline1['url'] == list_url and jline1['payload'] == payload:
+                    if jline1['url'] == list_url and jline1['payload'] == payload and jline1['number'] == number:
                         number_str = n - 1
                         break
             with open(AUTOSAVE_FILE, "r") as file:
@@ -73,10 +74,6 @@ class SendRequest(Thread):
                 file.writelines(lines2)
 
     def send_request(self, jline):
-        # logging.basicConfig(filename='requests.log', level=logging.INFO,
-                            # format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
-
-        # jline = json.loads(line.rstrip('\n'))
         list_url = CLIENTS.get(jline['client_id'])
         # получаем список адресов из конфига для отправки запросов
         if jline['client_id'] and jline['payload'] and jline['client_id'] in CLIENTS.keys():
@@ -99,23 +96,25 @@ class SendRequest(Thread):
                             time.sleep(time_size)
                             time_size *= 2
                             logging.info(
-                                f"Увеличиваем время отправки запроса {jline['payload']} на адрес {list_url[i]} до: {time_size} секунд")
+                                f"Увеличиваем время отправки запроса {jline['payload']} на адрес {list_url[i]} "
+                                f"до: {time_size} секунд")
                             response = requests.post(list_url[i], data=jline['payload'])
-                    #     logging.info(f"Запрос {jline['payload']} на адрес: {list_url[i]} успешно выполнен.")
-                    # else:
-                    #     logging.info(f"Запрос {jline['payload']} на адрес: {list_url[i]} успешно выполнен.")
-                    self.remove_request(list_url[i], jline['payload'])
-                    logging.info(f"Запрос {jline['payload']} на адрес: {list_url[i]} успешно выполнен. Строка {jline['number']} обработана")
+                    self.remove_request(list_url[i], jline['payload'], jline['number'])
+                    logging.info(
+                        f"Запрос {jline['payload']} на адрес: {list_url[i]} успешно выполнен. "
+                        f"Строка {jline['number']} обработана")
             self.next_request = False
             # переключаем флаг, чтобы следующий запрос мог уйти
         else:
-            logging.info(f"Обработка запроса: Client_ID: {jline['client_id']}, playload: {jline['payload']} - ошибка")
+            logging.info(f"Обработка запроса: Client_ID: {jline['client_id']}, payload: {jline['payload']} - ошибка")
+
 
 def http_post():
     # смотрим есть ли сохраненные неотправленные запросы, если да, то отправляем в один поток, не заморчаиваемся
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
     save_str = 0
-    # переменная в которой будет храниться максмальный номер строки, по которой уходили запросы
+    # переменная, в которой будет храниться максимальный номер обработанной строки
     try:
         # TODO: get rid of copy&paste
         with open(AUTOSAVE_FILE) as fp:
@@ -129,7 +128,9 @@ def http_post():
                     while response.status_code != 200:
                         time.sleep(time_size)
                         time_size *= 2
-                        logging.info(f"Увеличиваем время отправки запроса {jline['payload']} на адрес {jline['url']} до: {time_size} секунд")
+                        logging.info(
+                            f"Увеличиваем время отправки запроса {jline['payload']} на адрес {jline['url']} до: "
+                            f"{time_size} секунд")
                         response = requests.post(jline['url'], data=jline['payload'])
                 logging.info(f"Запрос {jline['payload']} на адрес: {jline['url']} успешно выполнен.")
 
